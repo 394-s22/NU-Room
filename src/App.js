330a647446c08e82d881c5d60d913ead7b1bbd67
Yemi Kelani <91276489+yemi-kelani@users.noreply.github.com>
2022-04-02 19:39:29 -0500

    Created Repo and Initialized Firebase

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> whats good 394?
         </p>
         <a
           className="App-link"
