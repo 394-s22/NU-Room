36d75f86ef1728348879d6e4c43f1a98668c803d
natbrew <57467420+natbrew@users.noreply.github.com>
2022-04-04 22:17:46 -0500

    profile styling into card

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
+import "../CSS/profile.css"
 
 // import { setData, useData } from "../utilities/firebase";
 
@@ -13,42 +14,45 @@ import Grid from "@mui/material/Grid";
 const Profile = ({ data }) => {
 
     return (
-        <Grid container spacing={2}>
-            <Grid item xs={6}>
-                <h1>First</h1>
-                
-            </Grid>
-            <Grid item xs={6}>
-                <h1>Last</h1>
-            </Grid>
 
-            <Grid item xs={12}>
-                <h2>Bio</h2>
-            </Grid>
-            <Grid item xs={12}>
-                <p>Hi my name is Name and I am from Place. I really enjoy hobbies. More words here.</p>
-            </Grid>
-            <Grid item xs={12}>
-                <img src="https://picsum.photos/200/300" alt="Profile Image"></img>
-            </Grid>
+        <div className="profileCard">
+            <Grid container spacing={2}>
+                <Grid item xs={6}>
+                    <h1>First</h1>
+                    
+                </Grid>
+                <Grid item xs={6}>
+                    <h1>Last</h1>
+                </Grid>
 
-            <Grid item xs={12}>
-                
-                    <h2>Hobbies</h2>
-                        <ul>
-                            <li>Drawing</li>
-                            <li>Reading</li>
-                        </ul>
-                        <h2>Personal Vibe</h2>
-                        <ul>
-                            <li>Introvert</li>
-                            <li>bookworm</li>
-                        </ul>
-                    <h2>___ % Match!</h2>
-                    <h3>Message Name at: @email</h3>
-                
+                <Grid item xs={12}>
+                    <h2>Bio</h2>
+                </Grid>
+                <Grid item xs={12}>
+                    <p>Hi my name is Name and I am from Place. I really enjoy hobbies. More words here.</p>
+                </Grid>
+                <Grid item xs={12}>
+                    <img id="profileImg" src="https://picsum.photos/200/300" alt="Profile Image"></img>
+                </Grid>
+
+                <Grid item xs={12}>
+                    
+                        <h2>Hobbies</h2>
+                            <ul>
+                                <li>Drawing</li>
+                                <li>Reading</li>
+                            </ul>
+                            <h2>Personal Vibe</h2>
+                            <ul>
+                                <li>Introvert</li>
+                                <li>bookworm</li>
+                            </ul>
+                        <h2>___ % Match!</h2>
+                        <h3>Message Name at: @email</h3>
+                    
+                </Grid>
             </Grid>
-        </Grid>
+        </div>
 
 
     );
