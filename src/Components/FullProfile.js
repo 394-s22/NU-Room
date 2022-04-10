54453a746a6093389d74a868ad35ae11ec1f7200
Josh <joshualee721@gmail.com>
2022-04-10 17:09:34 -0500

    added view full profile button

---
+++ src/Components/FullProfile.js
@@ -0,0 +1,10 @@
+import { useEffect, useState } from 'react';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Profile from "./Profile";
+import Grid from "@mui/material/Grid";
+import ImageList from "@mui/material/ImageList"
+import ImageListItem from "@mui/material/ImageListItem"
+import Masonry from '@mui/lab/Masonry';
+import Box from '@mui/material/Box';
+
