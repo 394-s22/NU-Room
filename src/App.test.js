8f50ddecbffc669bd4f4e6058e899b15008fb68e
Hugo Zhang <hugozhang2023@u.northwestern.edu>
2022-05-24 23:05:44 -0500

    bruh

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { useData } from './utilities/firebase.js';
 import App from './App';
 
-test('shows the NU Room Title', () => {
+test('shows the Loading screen', async () => {
   render(<App />);
-  const title = screen.getByText('NU Room');
-  expect(title).toBeInTheDocument();
+  const title = await screen.getAllByText('Loading...', {}, { timeout: 3000 })[0];
+  expect(title).toBeVisible();
 });
