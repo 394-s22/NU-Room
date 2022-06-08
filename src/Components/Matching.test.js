260d61d0ea47aff29dbf8b3819b0a05f7f5598d0
natbrew <nataliebrewster2023@u.northwestern.edu>
2022-06-07 19:17:41 -0700

    unit test 2

--- src/Components/Matching.test.js
+++ src/Components/Matching.test.js
@@ -11,9 +11,3 @@ test("Button to create new profile appears", async () => {
     render(<MatchesPage currentMatches={null} setCurrentProfile={()=>{}} setDisplayPage={()=>{}} currentMatchScores={()=>{}} matchesCommonalities={()=>{}} setMatchCommonalities={()=>{}}></MatchesPage>);
     expect(await screen.getByText('Create a profile')).toBeVisible();
 })
-
-// //hanyang@gmail.com
-// test("Full profile should have email displayed with correct text", async () => {
-//   render(<FullProfile profile={profile} setDisplayPage={()=>{}} commonalities={[]}></FullProfile>);
-//   expect(await screen.getByTestId('emailTest')).toBeVisible();
-// })
